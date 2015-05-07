99a025499028376e2d7c96b3a3a36578ff386231
Ehsan Khosroshahi <eb452@drexel.edu>
2015-05-07 15:05:50 -0400

    no message

--- FNIR Data - Day 1/EX2/E1.m
+++ FNIR Data - Day 1/EX2/E1.m
@@ -7,47 +7,90 @@ t=t-t(1);
 from = 1;
 to = find((round(t)==554));
 
-
+%size(t(from:to))
 
 
 fs = 2;
 filttype = 3; filtlen = 12; fcut = 0.14;
 
+figure
+hold on
+for i=3:2:15
+    data_lp=lpf(data(:,i),filttype,fs,fcut,filtlen);
+    
+    %plot(t(from:to),data(from:to,i))
 
-for i=3:2:10
-    data_lp=lpf(data(:,i),filttype,fs,fcut,filtlen);
-    figure
-    plot(t(from:to),data(from:to,i))
-    hold on
-    plot(t(from:to),data_lp(from:to),'r')
-    title(['oxy' num2str(i/2)])
+    plot(t(from:to),data_lp(from:to))
+    title(['oxy ' '1-' num2str(floor(i/2))])
 end
+legend('show')
+hold off
 
-for i=2:2:9
+figure
+hold on
+for i=15:2:33
     data_lp=lpf(data(:,i),filttype,fs,fcut,filtlen);
-    figure
-    plot(t(from:to),data(from:to,i))
-    hold on
-    plot(t(from:to),data_lp(from:to),'r')
-    title(['deoxy' (num2str(floor((i/2))))])
+    
+    %plot(t(from:to),data(from:to,i))
+ 
+    plot(t(from:to),data_lp(from:to))
+    title(['oxy ' num2str(floor(15/2)) '-16'])
 end
-for i=21:2:33
-    data_lp=lpf(data(:,i),filttype,fs,fcut,filtlen);
-    figure
-    plot(t(from:to),data(from:to,i))
-    hold on
-    plot(t(from:to),data_lp(from:to),'r')
-    title(['oxy' num2str(i/2)])
-end
+legend('show')
+hold off
 
-for i=20:2:33
+figure
+hold on
+for i=2:2:8
     data_lp=lpf(data(:,i),filttype,fs,fcut,filtlen);
-    figure
+    
     plot(t(from:to),data(from:to,i))
-    hold on
-    plot(t(from:to),data_lp(from:to),'r')
-    title(['deoxy' (num2str(floor((i/2))))])
+  
+    plot(t(from:to),data_lp(from:to))
+    title(['deoxy ' '1-' num2str(floor((i/2)))])
 end
+legend('show')
+hold off
+
+figure
+hold on
+for i=10:2:16
+    data_lp=lpf(data(:,i),filttype,fs,fcut,filtlen);
+    
+    %plot(t(from:to),data(from:to,i))
+  
+    plot(t(from:to),data_lp(from:to))
+    title(['deoxy ' '4-' num2str(floor((i/2)))])
+end
+legend('show')
+hold off
+
+figure 
+hold on 
+for i=18:2:24
+    data_lp=lpf(data(:,i),filttype,fs,fcut,filtlen);
+    
+    %plot(t(from:to),data(from:to,i))
+  
+    plot(t(from:to),data_lp(from:to))
+    title(['deoxy ' '9-' num2str(floor((i/2)))])
+end
+legend('show')
+hold off
+
+
+figure 
+hold on 
+for i=26:2:33
+    data_lp=lpf(data(:,i),filttype,fs,fcut,filtlen);
+    
+    %plot(t(from:to),data(from:to,i))
+    
+    plot(t(from:to),data_lp(from:to))
+    title(['deoxy' '12-' num2str(floor((i/2)))])
+end
+legend('show')
+hold off
 
 % 
 % for i=2:2:33
